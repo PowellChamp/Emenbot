47052fd5309dd85e31ab3dcea18fc5822abb7b64
Thomas Powell <goofytompo@gmail.com>
2022-06-11 23:17:13 +0100

    changed odds to 1 in 5

--- Main.py
+++ Main.py
@@ -34,7 +34,7 @@ async def on_message(message):
         "What's your problem with me?\nalways have to be an arsehole don't you"
     ]
     if str(message.author) == "BackRow#1214":
-        chance = random.randint(0,25)
+        chance = random.randint(0,5)
         print(chance)
         if chance == 1:
             await message.reply(random.choice(responses))
